6a0b15babe9eeeb3a304560c993416c83122a333
hcp17 <hernancortes170196@gmail.com>
2016-04-13 03:01:51 +0200

    Update resource.js

--- src/resource.js
+++ src/resource.js
@@ -1,5 +1,9 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
+    red: "res/Angry_Bird_red.png",
+    background: "res/Angry_birds_background_by_gsgill37-d3kogmx.png",
+    pig: "res/Minion_pig_copy.png",
+	box:"res/box.png"
 };
 
 var g_resources = [];
